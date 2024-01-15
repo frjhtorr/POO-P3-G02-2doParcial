8dc3cc540515d6f2bd3b9ca11823b22b12f09919
arcagarz <arcagarz@espol.edu.ec>
2024-01-15 15:27:45 -0500

    menu, ausp

--- src/main/java/com/edu/espol/aplicacion/MenuFeriasController.java
+++ src/main/java/com/edu/espol/aplicacion/MenuFeriasController.java
@@ -10,30 +10,122 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
+import com.espol.feria.*;
+import com.espol.personas.*;
+import com.espol.redes.*;
+import java.util.ArrayList;
+import javafx.event.ActionEvent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 /**
  * FXML Controller class
  *
  * @author Josh
  */
-/*
-Hola, este es un comentario de prueba
-*/
 public class MenuFeriasController implements Initializable {
 
     @FXML
     private Button buttonNuevaFeria;
     @FXML
-    private Button buttonVerFeria;
-    @FXML
     private VBox VBoxFerias;
-
+    @FXML
+    private Button VerAuspi;
+    // Comienzo a definir algunas ferias y a tener la lista
+    
+    public static Feria crearFeria(){
+        Feria feria = new Feria();
+        feria.setCodFeria("FE001");
+        feria.setNombre("Feria del Mueble 2024");
+        feria.setDescripcion("Venta de muebles de emprendimientos de guayaquileños");
+        feria.setLugar("Centro de convenciones");
+        feria.setFechaIni("2024-06-15");
+        feria.setFechaFin("2024-06-17");
+        feria.setHorario("10h00-14h00");
+        feria.setLugar("Centro de convenciones");
+        ArrayList<AuspicianteEnFeria> lstAuspiciantes= new ArrayList<>();
+        ArrayList<Emprendedor> lstEmprendedores= new ArrayList<>();
+        feria.setLstAuspiciantes(lstAuspiciantes);
+        feria.setLstEmprendedores(lstEmprendedores);
+        
+        Seccion[] seccionesFeria = new Seccion[4];
+        String[] letras = {"A","B","C","D"};
+        int j = 0;
+        for(String letra : letras){
+            Seccion seccion = new Seccion();
+            ArrayList<Stand> stands = new ArrayList<Stand>();
+            int cant = 4;
+            seccion.setCantStands(cant);
+            seccion.setId(letra);
+            for (int i = 1; i <= (cant+1); i++) {
+                Stand st = new Stand();
+                st.setCod(letra+i);
+                stands.add(st);
+            }
+            seccion.setArrayStands(stands);
+            seccionesFeria[j]=seccion;
+            j+=1;
+        }
+        feria.setSecciones(seccionesFeria);
+        
+        return feria;
+        
+        }
+    
+    
+       Feria feria1 = crearFeria();
+       ArrayList<Feria> ferias = new ArrayList<Feria>();
+ 
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        ferias.add(feria1);
+        VBoxFerias.setSpacing(10);
+       for(Feria f: ferias){ 
+          Button b = new Button("Ver info");
+          HBox hb1 = new HBox(new Text(f.getNombre()) , new Text(f.getCodFeria()), b);
+          hb1.setSpacing(20);
+         VBoxFerias.getChildren().add(hb1);
+         VBoxFerias.setSpacing(20);
+          // A partir de aqui hay el submenu que me lleva a ver la info de la feria
+          // Y las creaciones de botones que conecten con los demás menus
+          b.addEventHandler(MouseEvent.MOUSE_CLICKED, event ->{
+          Stage stfe = new Stage();
+          BorderPane bp = new BorderPane();
+          VBox feriavb = new VBox();
+          feriavb.getChildren().addAll(new Text("Nombre: "+f.getNombre()),new Text("Codigo: "+f.getCodFeria()) , new Text("Inicia: "+f.getFechaIni()+"Finaliza: "+f.getFechaFin()) , new Text("Con Horario: "+f.getHorario()) , new Text("Realizado en: "+f.getLugar()) );
+          Button btemp = new Button("Ver Emprendedores");
+          Button btaup = new Button("Ver Auspiciantes");
+          Button bteditfe = new Button("Editar Feria");
+          Button btstand = new Button("Ver Stands");
+          HBox hb = new HBox(btemp , btaup , bteditfe, btstand);
+          hb.setSpacing(25);
+          bp.setCenter(feriavb);
+          bp.setBottom(hb);
+          bp.setTop(new Text("Feria"));
+          stfe.setScene(new Scene(bp));
+          stfe.show();
+           });
+        
+         
+        }
+    }
+        
+
+   @FXML
+    private void switchToAuspiciantes(ActionEvent event) throws Exception{
+        App.setRoot("MenuVerAuspiciantes");
+        
+    }
     
-}
+    }
+
--- src/main/java/com/edu/espol/aplicacion/MenuVerAuspiciantesController.java
+++ src/main/java/com/edu/espol/aplicacion/MenuVerAuspiciantesController.java
@@ -10,25 +10,57 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
+import java.io.IOException;
+import com.espol.feria.*;
+import com.espol.personas.*;
+import com.espol.redes.*;
+import java.util.ArrayList;
+import javafx.fxml.FXML;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
 
-/**
- * FXML Controller class
- *
- * @author Josh
- */
-public class MenuVerAuspiciantesController implements Initializable {
+
+
+public class MenuVerAuspiciantesController implements Initializable{
 
     @FXML
     private Button buttonNuevoAuspiciante;
     @FXML
     private VBox vbAuspiciantes;
+    
+    ArrayList <Auspiciante> auspigen = new ArrayList<>();
+    Auspiciante au1 = crearAuspiciante();
+    
 
-    /**
-     * Initializes the controller class.
-     */
+     public Auspiciante crearAuspiciante(){
+        //Auspiciante 1
+        Auspiciante aus1 = new Auspiciante();
+        aus1.setCedula("9517538642");
+        aus1.setNombre("Christopher Lema");
+        aus1.setOwner("Auspicio 1");
+        aus1.setEmail("correo@gmail.com");
+        aus1.setWeb("www.ausp1.com.ec");
+        aus1.setTelefono("0952864928");
+        aus1.setDireccion("Local 1");
+        aus1.setSectorCubierto("ALIMENTACION");
+        ArrayList<CuentaRedSocial> lstRedes1 = new ArrayList<>();
+        CuentaRedSocial crs1 = new CuentaRedSocial(RedSocial.FACEBOOK,"Christopher Lema");
+        lstRedes1.add(crs1);
+        aus1.setLstRedes(lstRedes1);
+        return aus1;
+    }
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        auspigen.add(au1);
+        vbAuspiciantes.setSpacing(30);
+        vbAuspiciantes.getChildren().add(new Text("Numero de cedula"));
+        for(Auspiciante ausp: auspigen){ 
+          Button b = new Button("Ver info");        
+          HBox hb1 = new HBox(new Text("Nombre: "+ausp.getNombre()), new Text(ausp.getCedula()), b);
+          hb1.setSpacing(10);
+          vbAuspiciantes.getChildren().add(hb1);
+    } 
+}
     
 }
